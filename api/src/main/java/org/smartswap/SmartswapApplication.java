b2ac8f98c40ce218431099ea9843ca59d60c9351
Nicolas Potel <potelnicolasperso@gmail.com>
2025-09-09 15:46:12 +0200

    🎉 Clickhouse logs database & Sprin Boot API creation

---
+++ api/src/main/java/org/smartswap/SmartswapApplication.java
@@ -0,0 +1,13 @@
+package org.smartswap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartswapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartswapApplication.class, args);
+    }
+
+}
